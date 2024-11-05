90def5cf2ecf4106b92800dd09808f6ce654fe1f
Hassan Agmir <hagmir6@gmail.com>
2024-11-05 20:44:47 +0100

    new updates

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,7 +4,7 @@
 @section('content')
 <div class="max-w-2xl m-auto">
     <div class="col-lg-12 px-3">
-        <h1 class="text-3xl font-bold mt-2" id="send-us-a-message">Contact me</h1>
+        <h1 class="text-3xl font-bold my-2" id="send-us-a-message">Contact me</h1>
         <div>
             <p>I'd love to hear from you! Whether you have questions about my services, I'm here to help.</p>
             <h2 class="text-xl font-bold mt-2" id="get-in-touch">Get in Touch</h2>
